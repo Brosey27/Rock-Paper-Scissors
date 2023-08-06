2625f7c9b32f1ce3c600b9d36614ca361d5168f1
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-06 21:37:06 +1000

    Add 3 const for player Text, computerText and result Text with corresponding queryselectors selecting the assigned ids.Add cont choiceButton with query selector all selecting the choicebutton class. Add let player, computer and result

---
+++ index.js
@@ -0,0 +1,7 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceButtons = document.querySelectorAll(".choiceButton");
+let player;
+let computer;
+let result;
