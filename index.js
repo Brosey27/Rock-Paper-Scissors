5e3587ba13e493d9426a35249e453ce39a244875
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-06 21:47:12 +1000

    Add ChoiceButtons for each, addEventlistener click.  Add player = button.textContent and computerTurn function to have a const of random number and a switch for the random number

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const choiceButtons = document.querySelectorAll(".choiceButton");
 let player;
 let computer;
 let result;
+
+choiceButtons.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+}));
+
+function computerTurn(){
+    
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum)
+}
